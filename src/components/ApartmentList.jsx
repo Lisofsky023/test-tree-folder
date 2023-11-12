f8f04f0e34149407d38a05e30ff1de41769c0f25
Lisofsky023 <lisa.2023.ge@gmail.com>
2023-11-13 01:07:34 +0400

    first commit

--- src/components/ApartmentList.jsx
+++ src/components/ApartmentList.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { fetchApartments, fetchClients } from '../api/clientApi';
+import { fetchApartments } from '../api/clientApi';
 import PropTypes from 'prop-types';
 
 const ApartmentList = ({ houseId, onApartmentSelect }) => {
@@ -12,43 +12,26 @@ const ApartmentList = ({ houseId, onApartmentSelect }) => {
     setLoading(true);
     setError(null);
   
-    Promise.all([fetchApartments(houseId), fetchClients()])
-      .then(([apartmentData, allClients]) => {
-        console.log('Apartments data:', apartmentData);
-        console.log('Clients data:', allClients);
+    fetchApartments(houseId)
+      .then(apartmentData => {
         setApartments(apartmentData);
-        const filteredClients = allClients.filter(client => {
-          return apartmentData.some(apartment => {
-            return apartment.addressId === client.bindId;
-          });
-        });
-        setClients(filteredClients);
       })
       .catch(err => {
-        setError(err);
+        setError(err.message);
         console.error('Ошибка при загрузке данных:', err);
       })
       .finally(() => setLoading(false));
   }, [houseId]);
 
-
-  const handleApartmentClick = (apartment) => {
-    onApartmentSelect(apartment);
-  };
-
   return (
     <div>
       <h2>Apartments</h2>
-      <ul>
-        {apartments.map(apartment => (
-          <li key={apartment.addressId || apartment.flat} onClick={() => handleApartmentClick(apartment)}>
-            Квартира {apartment.flat}
-            <ul>
-              {clients.filter(client => client.BindId === apartment.AddressId)
-                      .map(client => <li key={client.Id}>Жилец: {client.Name}</li>)}
-            </ul>
-          </li>
-        ))}
+        <ul>
+          {apartments.map(apartment => (
+            <li key={apartment.addressId || apartment.flat} onClick={() => onApartmentSelect(apartment)}>
+              Квартира {apartment.flat}
+            </li>
+          ))}
       </ul>
     </div>
   );
